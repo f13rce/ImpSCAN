69d48e5a2c0a7d75a09594852c74d21ada4922c5
f13rce <f13rce@protonmail.com>
2019-07-01 21:25:29 +0100

    Documentation stuff for self

---
+++ example/netlib/sys.py
@@ -0,0 +1,8 @@
+# This is purely a test to see how this affects the import of netlibclient.py
+
+hasEffectivePythonCode = True
+
+def FillerFunction():
+	hasEffectivePythonCode = False
+
+FillerFunction()
